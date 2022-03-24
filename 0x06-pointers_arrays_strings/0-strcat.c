4022029fd9573be8ff35072f3e9318d604739adc
unknown <Andualemasamenew5@gmail.com>
2022-03-24 13:42:30 +0300

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,36 +1,17 @@
-#include "main.h"
-	
+tenates src to end of dest
+ * Return: pointer to dest
+ **/
 
-	/**
-	 * _strcat - concatenates two strings
-	 * @src: second string to copy from
-	 * @dest: string to be overwritten
-	 * Description: concatenates src to end of dest
-	 * Return: pointer to dest
-	 **/
-	
+char *_strcat(char *dest, char *src)
+{
+        int i, j;
 
-	char *_strcat(char *dest, char *src)
-	{
-		int i, j;
-	
+        i = 0;
 
-		i = 0;
-	
+        while (dest[i] != '\0')
+                i++;
 
-		while (dest[i] != '\0')
-			i++;
-	
+        for (j = 0; src[j] != '\0'; j++)
+        {
+                dest[i
 
-		for (j = 0; src[j] != '\0'; j++)
-		{
-			dest[i] = src[j];
-			i++;
-		}
-	
-
-		dest[i] = src[j];
-	
-
-		return (dest);
-	}
