523e301963046aaec451a04e12beffd1dca4b5ae
unknown <Andualemasamenew5@gmail.com>
2022-03-24 14:41:04 +0300

    100-rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,21 @@
 #include "main.h"
 
 /**
- * rot13 -  a function that encodes a string using rot13.
- * @s: An input string to encode using rot13
- * Return: An encode string
- */
-char *rot13(char *s)
-{
-	int i = 0;
+ * rot13 - encode string with rot13
+ * @src: string to encode
+ * Return: string
+ **/
 
-	while (s[i] != '\0')
+char *rot13(char *src)
+{
+	char c[] = {"NOPQRSTUVWXYZABCDEFGHIJKLM[\\]^_`nopqrstuvwxyzabcdefghijklm"};
+
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		while ((s[i] >= 'a' && s[i] <= 'z') ||
-				(s[i] >= 'A' && s[i] <= 'Z'))
-		{
-			if ((s[i] >= 'a' && s[i] <= 'm') ||
-					(s[i] >= 'A' && s[i] <= 'M'))
-				s[i] += 13;
-			else
-				s[i] -= 13;
-			i++;
-		}
-		i++;
+		if (src[i] >= 'A' && src[i] <= 'z')
+			src[i] = c[src[i] - 'A'];
 	}
-	return (s);
+	return (src);
 }
