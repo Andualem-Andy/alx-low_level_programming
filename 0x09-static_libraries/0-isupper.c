a6aa75d79b20581d4f0931ae08427b4b2942e86d
unknown <Andualemasamenew5@gmail.com>
2022-04-04 15:37:48 +0300

    update

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper - check the code for Holberton School students.
- *
- * @c: is an integer param
- *
+ * _isupper - Check if paramter c represents an uppercase letter.
+ * @c: Int representing a character
  * Return: Always 0.
  */
-
-
 int _isupper(int c)
 {
-
-	return (c >= 65 && c <= 90);
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
 }
