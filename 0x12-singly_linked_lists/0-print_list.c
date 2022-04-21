43d1b851fdec8f1299eb3550d5c835b9361cde5c
unknown <Andualemasamenew5@gmail.com>
2022-04-21 14:49:06 +0300

    0-print

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,45 @@
 #include "lists.h"
-/**
- * print_list - prints all the elements of a list_t list.
- * @h: singly linked list.
- * Return: number of elements in the list.
- */
+#include <stdio.h>
 
+/**
+	* print_list - prints all elements of a linked list
+	* @h: head of linked list
+	* Description: prints all elements of a linked list
+	* Return: size_t
+*/
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	list_t current;
+	size_t length = 0;
 
-	nelem = 0;
-	while (h != NULL)
+	if (h == NULL)
+		return (length);
+
+	current = *h;
+
+	while (current.next != NULL)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		nelem++;
+		if (current.str == NULL)
+		{
+			printf("[%i] %s\n", 0, "(nil)");
+			current = *current.next;
+			length++;
+			continue;
+		}
+		printf("[%i] %s\n", current.len, current.str);
+		length++;
+		current = *current.next;
 	}
-	return (nelem);
+
+	if (current.str == NULL)
+	{
+		printf("[%i] %s\n", 0, "(nil)");
+		length++;
+	}
+	else
+	{
+		printf("[%i] %s\n", current.len, current.str);
+	}
+
+	return (length + 1);
 }
